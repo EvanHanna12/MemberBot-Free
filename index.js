8292487ada8b540fe61a756fe3138d99385155f9
ikeroy <32038178+ikeroy@users.noreply.github.com>
2020-08-18 19:49:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
-const Discord = require("discord.js")
+const Discord = require("discord.js");
+const { EventEmitter } = require('events');
 const client = new Discord.Client();
 const gaming_file = '../Servers/gaming.json'
-const music_file = '../Server/music.json'
-const chill_out_file = '../Server/chill_out.json'
-const giveaways_file = '../Server/givaways.json'
-const programming_file = '../Server/programming.json'
-const learning_file = '../Server/learning.json'
-const other_file = '../Server/other.json'
+const music_file = '../Servers/music.json'
+const chill_out_file = '../Servers/chill_out.json'
+const giveaways_file = '../Servers/giveaways.json'
+const programming_file = '../Servers/programming.json'
+const learning_file = '../Servers/learning.json'
+const other_file = '../Servers/other.json'
+
+
+
+require('events').EventEmitter.defaultMaxListeners = 15;
 
 var embed_main = {
     "title": "Server list",
@@ -95,245 +100,81 @@ client.on("message", msg => {
     }
 })
 
+
+
+
 // sp!setup-category
 // creates an invite to the guild channel where the command is run
 // saves the guild name and invite to file
 // if guild name was already in the file it is updated
 client.on("message", msg => {
+    function Setup(file) {
+
+        // get the existing guilds saved to file
+        var existingGuilds = JSON.parse(fs.readFileSync(file, 'utf-8'))
+    
+        // get the index of the guild (it it already exists)
+        var guildIndex = existingGuilds.fields.indexOf(function (g) {
+            return msg.guild.name === g.name
+        })
+        console.log("Exitsing guild at index " + guildIndex)
+        // if the guild doesn't exist, 
+        // create an entry for this guild with a blank invite
+        // and add it to the list of existing guilds
+        if (guildIndex === -1) {
+            guildIndex = existingGuilds.fields.push({
+                "name": msg.guild.name,
+                "value": ""
+            })
+            guildIndex -= 1
+            console.log("created guild at index " + guildIndex)
+        }
+    
+        // update guild with new invite, save it, and send details in reply
+        msg.channel.createInvite({ maxAge: 0 })
+            .then(invite => {
+                console.log("updating invite for guild at index " + guildIndex)
+                console.log("length of existing guilds " + existingGuilds.fields.length)
+                guild = existingGuilds.fields[guildIndex]
+                console.log(guild)
+                guild.value = invite.url
+                fs.writeFileSync(file, JSON.stringify(existingGuilds), 'utf-8')
+                msg.reply('Success!')
+            })
+    }
+    
     if (msg.content === "sp!setup-gaming") {
-
-        // get the existing guilds saved to file
-        var existingGuilds = JSON.parse(fs.readFileSync(gaming_file, 'utf-8'))
-
-        // get the index of the guild (it it already exists)
-        var guildIndex = existingGuilds.fields.indexOf(function (g) {
-            return msg.guild.name === g.name
-        })
-        console.log("Exitsing guild at index " + guildIndex)
-        // if the guild doesn't exist, 
-        // create an entry for this guild with a blank invite
-        // and add it to the list of existing guilds
-        if (guildIndex === -1) {
-            guildIndex = existingGuilds.fields.push({
-                "name": msg.guild.name,
-                "value": ""
-            })
-            guildIndex -= 1
-            console.log("created guild at index " + guildIndex)
-        }
-
-        // update guild with new invite, save it, and send details in reply
-        msg.channel.createInvite({ maxAge: 0 })
-            .then(invite => {
-                console.log("updating invite for guild at index " + guildIndex)
-                console.log("length of existing guilds " + existingGuilds.fields.length)
-                guild = existingGuilds.fields[guildIndex]
-                console.log(guild)
-                guild.value = invite.url
-                fs.writeFileSync(gaming_file, JSON.stringify(existingGuilds), 'utf-8')
-                msg.reply('Success!')
-            })
+        Setup(gaming_file)
+    } else if (msg.content === "sp!setup-music") {
+        Setup(music_file)
+    } else if (msg.content === "sp!setup-chill out") {
+        Setup(chill_out_file)
+    } else if (msg.content === "sp!setup-giveaways") {
+        Setup(giveaways_file)
+    } else if (msg.content === "sp!setup-programming") {
+        Setup(programming_file)
+    } else if (msg.content === "sp!setup-other") {
+        Setup(other_file)
     }
-
-}
-)
-client.on("message", msg => {
-    if (msg.content === "sp!setup-music") {
-
-        // get the existing guilds saved to file
-        var existingGuilds = JSON.parse(fs.readFileSync(gaming_file, 'utf-8'))
-
-        // get the index of the guild (it it already exists)
-        var guildIndex = existingGuilds.fields.indexOf(function (g) {
-            return msg.guild.name === g.name
-        })
-        console.log("Exitsing guild at index " + guildIndex)
-        // if the guild doesn't exist, 
-        // create an entry for this guild with a blank invite
-        // and add it to the list of existing guilds
-        if (guildIndex === -1) {
-            guildIndex = existingGuilds.fields.push({
-                "name": msg.guild.name,
-                "value": ""
-            })
-            guildIndex -= 1
-            console.log("created guild at index " + guildIndex)
-        }
-
-        // update guild with new invite, save it, and send details in reply
-        msg.channel.createInvite({ maxAge: 0 })
-            .then(invite => {
-                console.log("updating invite for guild at index " + guildIndex)
-                console.log("length of existing guilds " + existingGuilds.fields.length)
-                guild = existingGuilds.fields[guildIndex]
-                console.log(guild)
-                guild.value = invite.url
-                fs.writeFileSync(music_file, JSON.stringify(existingGuilds), 'utf-8')
-                msg.reply('Success!')
-            })
-    }
-
-}
-)
-client.on("message", msg => {
-    if (msg.content === "sp!setup-chill out") {
-
-        // get the existing guilds saved to file
-        var existingGuilds = JSON.parse(fs.readFileSync(chill_out_file, 'utf-8'))
-
-        // get the index of the guild (it it already exists)
-        var guildIndex = existingGuilds.fields.indexOf(function (g) {
-            return msg.guild.name === g.name
-        })
-        console.log("Exitsing guild at index " + guildIndex)
-        // if the guild doesn't exist, 
-        // create an entry for this guild with a blank invite
-        // and add it to the list of existing guilds
-        if (guildIndex === -1) {
-            guildIndex = existingGuilds.fields.push({
-                "name": msg.guild.name,
-                "value": ""
-            })
-            guildIndex -= 1
-            console.log("created guild at index " + guildIndex)
-        }
-
-        // update guild with new invite, save it, and send details in reply
-        msg.channel.createInvite({ maxAge: 0 })
-            .then(invite => {
-                console.log("updating invite for guild at index " + guildIndex)
-                console.log("length of existing guilds " + existingGuilds.fields.length)
-                guild = existingGuilds.fields[guildIndex]
-                console.log(guild)
-                guild.value = invite.url
-                fs.writeFileSync(chill_out_file, JSON.stringify(existingGuilds), 'utf-8')
-                msg.reply('Success!')
-            })
-    }
-
 }
 )
 
-client.on("message", msg => {
-    if (msg.content === "sp!setup-giveaways") {
-
-        // get the existing guilds saved to file
-        var existingGuilds = JSON.parse(fs.readFileSync(giveaways_file, 'utf-8'))
-
-        // get the index of the guild (it it already exists)
-        var guildIndex = existingGuilds.fields.indexOf(function (g) {
-            return msg.guild.name === g.name
-        })
-        console.log("Exitsing guild at index " + guildIndex)
-        // if the guild doesn't exist, 
-        // create an entry for this guild with a blank invite
-        // and add it to the list of existing guilds
-        if (guildIndex === -1) {
-            guildIndex = existingGuilds.fields.push({
-                "name": msg.guild.name,
-                "value": ""
-            })
-            guildIndex -= 1
-            console.log("created guild at index " + guildIndex)
-        }
-
-        // update guild with new invite, save it, and send details in reply
-        msg.channel.createInvite({ maxAge: 0 })
-            .then(invite => {
-                console.log("updating invite for guild at index " + guildIndex)
-                console.log("length of existing guilds " + existingGuilds.fields.length)
-                guild = existingGuilds.fields[guildIndex]
-                console.log(guild)
-                guild.value = invite.url
-                fs.writeFileSync(giveaways_file, JSON.stringify(existingGuilds), 'utf-8')
-                msg.reply('Success!')
-            })
-    }
-
-}
-)
-
-client.on("message", msg => {
-    if (msg.content === "sp!setup-programming") {
-
-        // get the existing guilds saved to file
-        var existingGuilds = JSON.parse(fs.readFileSync(programming_file, 'utf-8'))
-
-        // get the index of the guild (it it already exists)
-        var guildIndex = existingGuilds.fields.indexOf(function (g) {
-            return msg.guild.name === g.name
-        })
-        console.log("Exitsing guild at index " + guildIndex)
-        // if the guild doesn't exist, 
-        // create an entry for this guild with a blank invite
-        // and add it to the list of existing guilds
-        if (guildIndex === -1) {
-            guildIndex = existingGuilds.fields.push({
-                "name": msg.guild.name,
-                "value": ""
-            })
-            guildIndex -= 1
-            console.log("created guild at index " + guildIndex)
-        }
-
-        // update guild with new invite, save it, and send details in reply
-        msg.channel.createInvite({ maxAge: 0 })
-            .then(invite => {
-                console.log("updating invite for guild at index " + guildIndex)
-                console.log("length of existing guilds " + existingGuilds.fields.length)
-                guild = existingGuilds.fields[guildIndex]
-                console.log(guild)
-                guild.value = invite.url
-                fs.writeFileSync(programming_file, JSON.stringify(existingGuilds), 'utf-8')
-                msg.reply('Success!')
-            })
-    }
-
-}
-)
-
-client.on("message", msg => {
-    if (msg.content === "sp!setup-other") {
-
-        // get the existing guilds saved to file
-        var existingGuilds = JSON.parse(fs.readFileSync(other_file, 'utf-8'))
-
-        // get the index of the guild (it it already exists)
-        var guildIndex = existingGuilds.fields.indexOf(function (g) {
-            return msg.guild.name === g.name
-        })
-        console.log("Exitsing guild at index " + guildIndex)
-        // if the guild doesn't exist, 
-        // create an entry for this guild with a blank invite
-        // and add it to the list of existing guilds
-        if (guildIndex === -1) {
-            guildIndex = existingGuilds.fields.push({
-                "name": msg.guild.name,
-                "value": ""
-            })
-            guildIndex -= 1
-            console.log("created guild at index " + guildIndex)
-        }
-
-        // update guild with new invite, save it, and send details in reply
-        msg.channel.createInvite({ maxAge: 0 })
-            .then(invite => {
-                console.log("updating invite for guild at index " + guildIndex)
-                console.log("length of existing guilds " + existingGuilds.fields.length)
-                guild = existingGuilds.fields[guildIndex]
-                console.log(guild)
-                guild.value = invite.url
-                fs.writeFileSync(other_file, JSON.stringify(existingGuilds), 'utf-8')
-                msg.reply('Success!')
-            })
-    }
-
-}
-)
-
-client.on("guildCreate", (guild) => {
+client.on("guildCreate", guild => {
     console.log(`Joined new guild: ${guild.name}`);
 })
+
+client.on('guildMemberAdd', member => {
+    
+    // get the existing guilds saved to file
+    var existingGuilds = JSON.parse(fs.readFileSync(file, 'utf-8'))
+
+    // get the index of the guild (it it already exists)
+    var guild = existingGuilds.fields.find(function (g) {
+        return member.guild.name === g.name
+    })
+
+    guild.value 
+});
 
 //client.on("guildCreate", (guild) => {
 //    if (Discord.Invite.inviter === client.user.tag) {
